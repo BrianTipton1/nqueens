bf8c917246e7b7fa23bdb57a146bfa87c373c764
Brian Tipton <briantiptondev@gmail.com>
2023-09-24 04:34:57 -0500

    good spot

--- app/Main.hs
+++ app/Main.hs
@@ -1,35 +1,483 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
+import Control.Monad (forM, replicateM)
 import Data.Bifunctor (bimap)
+import Data.Data (Typeable)
+import Data.Foldable (Foldable (foldl'), find)
+import Data.Function (on)
+import Data.Functor ((<&>))
+import Data.List (isSubsequenceOf, nub, sortBy, sortOn)
+import Data.Maybe (fromMaybe)
+import System.Environment (getArgs)
+import System.Exit (ExitCode (ExitFailure, ExitSuccess), exitSuccess, exitWith)
+import System.Process (system)
+import System.Random (Random (randoms), newStdGen, randomRIO)
+import Text.Read (readMaybe)
 
-newtype Queen = Queen {queen :: Int} deriving (Show)
-newtype Square = Square {square :: Maybe Queen} deriving (Show)
-newtype Board = Board {board :: [[Square]]} deriving (Show)
+--- Helpers
+redifyString :: String -> String
+redifyString s = "\ESC[31m" ++ s ++ "\ESC[0m"
 
-newtype Row = Row {row :: Int} deriving (Show)
-newtype Column = Column {column :: Int} deriving (Show)
-newtype Coordinate = Coordinate {coordinate :: (Row, Column)} deriving (Show)
+greenifyString :: String -> String
+greenifyString str = "\x1b[32m" ++ str ++ "\x1b[0m"
 
-coordinateValue :: Coordinate -> (Int, Int)
-coordinateValue = bimap row column . coordinate
-rowValue :: Coordinate -> Int
-rowValue = row . fst . coordinate
-columnValue :: Coordinate -> Int
-columnValue = column . snd . coordinate
-
-initializaNBoard :: Int -> [[Square]]
-initializaNBoard n = initializaNBoard' [] n
+split :: Char -> String -> [String]
+split _ "" = [""]
+split delimiter (x : xs)
+  | x == delimiter = "" : rest
+  | otherwise = (x : head rest) : tail rest
  where
-  initializaNBoard' acc n
-    | length acc == n = acc
-    | otherwise = initializaNBoard' (inializaNRow [] n : acc) n
-  inializaNRow :: [Square] -> Int -> [Square]
-  inializaNRow acc n
-    | length acc == n = acc
-    | otherwise = inializaNRow (Square Nothing : acc) n
+  rest = split delimiter xs
+
+---
+
+-- Cmd Line Stuff
+data CmdInitBoard
+  = CmdRandom
+  | CmdDiagonal
+  deriving (Eq, Show)
+
+data CommandLineOption
+  = CmdHelp
+  | CmdInitBoard CmdInitBoard
+  | CmdSize Int
+  | CmdCrossover Double
+  | CmdMutation Double
+  | CmdPopulation Int
+  | CmdGeneration Int
+  deriving (Eq, Show)
+
+cmdHelpList :: [String]
+cmdHelpList = ["-h", "--help"]
+cmdDiagonal :: [String]
+cmdDiagonal = ["-d", "--diagonal"]
+cmdRandom :: [String]
+cmdRandom = ["-r", "--random"]
+cmdSize :: String -> [String]
+cmdSize s = (++ s) <$> ["-s=", "--size="]
+cmdCrossover :: String -> [String]
+cmdCrossover s = (++ s) <$> ["-c=", "--crossover="]
+cmdMutation :: String -> [String]
+cmdMutation s = (++ s) <$> ["-m=", "--mutation="]
+cmdPopulation :: String -> [String]
+cmdPopulation s = (++ s) <$> ["-p=", "--population="]
+cmdGeneration :: String -> [String]
+cmdGeneration s = (++ s) <$> ["-g=", "--generatin="]
+
+strToOps :: String -> IO CommandLineOption
+strToOps s
+  | s `elem` cmdHelpList = return CmdHelp
+  | s `elem` cmdRandom = return $ CmdInitBoard CmdRandom
+  | s `elem` cmdDiagonal = return $ CmdInitBoard CmdDiagonal
+  | s `elem` cmdSize size && isType @Int size = return $ CmdSize (read size :: Int)
+  | s `elem` cmdCrossover size && isType @Double size = return $ CmdCrossover (read size :: Double)
+  | s `elem` cmdMutation size && isType @Double size = return $ CmdMutation (read size :: Double)
+  | s `elem` cmdPopulation size && isType @Int size = return $ CmdPopulation (read size :: Int)
+  | s `elem` cmdGeneration size && isType @Int size = return $ CmdGeneration (read size :: Int)
+  | otherwise = return CmdHelp
+ where
+  isType :: forall a. (Read a, Typeable a) => String -> Bool
+  isType s = case readMaybe s :: Maybe a of
+    Just _ -> True
+    Nothing -> False
+  size = last $ split '=' s
+
+checkOps :: [CommandLineOption] -> IO [CommandLineOption]
+checkOps ops
+  | containsHelp ops
+      || null ops
+      || isSubsequenceOf [CmdInitBoard CmdRandom, CmdInitBoard CmdDiagonal] ops =
+    do
+      putStrLn helpScreen
+      exitWith $ ExitFailure 1
+  | otherwise = return ops
+ where
+  containsHelp = elem CmdHelp
+
+helpScreen :: String
+helpScreen =
+  unlines
+    [ "N-Queens Solver"
+    , "====================="
+    , ""
+    , "Usage:"
+    , "    nqueens [-s=SIZE] [-r | -d] [-c=PROB] [-m=PROB] [-p=SIZE] [-g=GEN]"
+    , ""
+    , "Description:"
+    , "    This program solves the N-Queens problem, which involves placing N chess queens on an N×N chessboard so that no two queens threaten each other."
+    , ""
+    , "Options:"
+    , "    -s=SIZE, --size=SIZE          Specifies the size of the board. SIZE must be an integer greater than 3. (Default: 8)"
+    , "    "
+    , "    -r, --random                  Solves the problem by placing queens at random positions that do not conflict with each other."
+    , "    "
+    , "    -d, --diagonal                Solves the problem by placing queens along the diagonal of the board before attempting to solve."
+    , "    "
+    , "    -c=PROB, --crossover=PROB     Specifies the crossover probability. PROB must be a decimal between 0 and 1."
+    , "    "
+    , "    -m=PROB, --mutation=PROB      Specifies the mutation probability. PROB must be a decimal between 0 and 1."
+    , "    "
+    , "    -p=SIZE, --population=SIZE    Specifies the population size. SIZE must be an integer."
+    , "    "
+    , "    -g=GEN, --generation=GEN      Specifies the number of generations. GEN must be an integer."
+    , "  "
+    , "Note:"
+    , "    Either the -r/--random flag or the -d/--diagonal flag must be supplied; both cannot be used at the same time."
+    , ""
+    , "Examples:"
+    , "    nqueens -r                    # With default size of 8"
+    , "    nqueens -s=8 -r"
+    , "    nqueens --size=10 --diagonal"
+    , "    nqueens -s=10 -r -c=0.7 -m=0.2 -p=50 -g=100"
+    ]
+
+getDefaultOrOption :: [CommandLineOption] -> CommandLineOption -> CommandLineOption
+getDefaultOrOption opts defaultOpt =
+  fromMaybe defaultOpt (find (matchesType defaultOpt) opts)
+ where
+  matchesType :: CommandLineOption -> CommandLineOption -> Bool
+  matchesType CmdHelp CmdHelp = True
+  matchesType (CmdInitBoard CmdDiagonal) (CmdInitBoard CmdDiagonal) = True
+  matchesType (CmdInitBoard CmdRandom) (CmdInitBoard CmdRandom) = True
+  matchesType (CmdSize _) (CmdSize _) = True
+  matchesType (CmdCrossover _) (CmdCrossover _) = True
+  matchesType (CmdMutation _) (CmdMutation _) = True
+  matchesType (CmdPopulation _) (CmdPopulation _) = True
+  matchesType (CmdGeneration _) (CmdGeneration _) = True
+  matchesType _ _ = False
+
+processInitBoard :: [CommandLineOption] -> Int -> Int -> IO Population
+processInitBoard opts size popsize = do
+  let initBoardOpt = filter isCmdInitBoard opts
+  case initBoardOpt of
+    (CmdInitBoard b : _) -> handleInitBoard b
+    _ -> handleInitBoard CmdDiagonal
+ where
+  isCmdInitBoard (CmdInitBoard _) = True
+  isCmdInitBoard _ = False
+  handleInitBoard CmdRandom = replicateM popsize $ randomNSizeBoard size
+  handleInitBoard CmdDiagonal = return $ replicate popsize $ createDiagonalBoard size
+
+-- End Cmd Line stuff
+
+--- Types Relating to the Assignment
+
+-- Defs
+newtype Queen = Queen {queen :: Int} deriving (Show)
+
+newtype Rank = Rank {rank :: Int} deriving (Show, Eq)
+newtype File = File {file :: Int} deriving (Show, Eq)
+newtype Coordinate = Coordinate {coordinate :: (Rank, File)} deriving (Show, Eq)
+
+newtype To = To {to :: Coordinate} deriving (Show)
+newtype From = From {from :: Coordinate} deriving (Show)
+newtype Move = Move {move :: (From, To)} deriving (Show)
+newtype LookupRow = LookupRow {lookupRow :: (Queen, Coordinate)}
+
+type Row = Int
+type Column = Int
+type Dims = (Row, Column)
+data LookupTable = LookupTable
+  { lookupTable :: ![LookupRow]
+  , deminsions :: !Dims
+  }
+type MutationRate = Double
+type CrossoverRate = Double
+type BoardSize = Int
+type Population = [LookupTable]
+
+data Configuration = Configuration
+  { boardSize :: Int
+  , popSize :: Int
+  , maxGen :: Int
+  , generationSize :: Int
+  , mutationRate :: Double
+  , crossOverRate :: Double
+  }
+
+-- End Defs
+
+--Instances
+instance Show LookupTable where
+  show :: LookupTable -> String
+  show (LookupTable lookupTbl (row, col)) =
+    unlines (map (concatMap prettyPrint) rows)
+   where
+    rows = [[(r, c) | c <- [1 .. col]] | r <- [1 .. row]]
+    lookupMap = [(coordinateValue coord, queen q) | LookupRow (q, coord) <- lookupTbl]
+    prettyPrint (r, c) = case lookup (r, c) lookupMap of
+      Just n -> colorize (r, c) $ "| " ++ show n ++ " |"
+      Nothing -> colorize (r, c) "| _ |"
+
+    colorize (r, c) str
+      | even (r + c) = redifyString str
+      | otherwise = greenifyString str
+
+instance Show LookupRow where
+  show :: LookupRow -> String
+  show (LookupRow (Queen q, Coordinate (Rank r, File c))) =
+    "Queen " ++ show q ++ " at (" ++ show r ++ ", " ++ show c ++ ")"
+
+instance Show Configuration where
+  show :: Configuration -> String
+  show
+    Configuration
+      { boardSize = brdSize
+      , popSize = popSize
+      , generationSize = currGen
+      , maxGen = maxGen
+      , mutationRate = mutRate
+      , crossOverRate = cRate
+      } =
+      "Board Size: " ++ show brdSize ++ " | "
+        ++ "Population Size: "
+        ++ show popSize
+        ++ " | "
+        ++ "Generation Number: "
+        ++ show currGen
+        ++ " | "
+        ++ "Max Generations: "
+        ++ show maxGen
+        ++ " | "
+        ++ "Mutation Rate: "
+        ++ show (100 * mutRate)
+        ++ "% | "
+        ++ "Crossover Rate: "
+        ++ show (100 * cRate)
+        ++ "%"
+
+--Instances
+
+--- End types Relating to the Assignment
+
+queensThreaten :: Coordinate -> Coordinate -> Bool
+queensThreaten (Coordinate (Rank r1, File c1)) (Coordinate (Rank r2, File c2)) =
+  r1 == r2 || c1 == c2 || abs (r1 - r2) == abs (c1 - c2)
+
+numThreats :: LookupTable -> Int
+numThreats (LookupTable lookupTbl dims) = countThreats coords
+ where
+  coords = map (snd . lookupRow) lookupTbl
+  countThreats [] = 0
+  countThreats (x : xs) = length (filter (queensThreaten x) xs) + countThreats xs
+
+coordinatesBetween :: Coordinate -> Coordinate -> [Coordinate]
+coordinatesBetween (Coordinate (Rank r1, File c1)) (Coordinate (Rank r2, File c2)) =
+  [Coordinate (Rank r, File c) | r <- [min r1 r2 + 1 .. max r1 r2 - 1], c <- [min c1 c2 + 1 .. max c1 c2 - 1]]
+
+isPathEmpty :: Coordinate -> Coordinate -> [LookupRow] -> Bool
+isPathEmpty from to lookupTbl =
+  all (\c -> c `notElem` map (snd . lookupRow) lookupTbl) (coordinatesBetween from to)
+
+moveQueenInLookup :: Move -> LookupTable -> Maybe LookupTable
+moveQueenInLookup (Move (From from, To to)) (LookupTable lookupTbl dims) =
+  let maybeQueen = lookupByCoordinate from lookupTbl
+   in case maybeQueen of
+        Just queen ->
+          if isPathEmpty from to lookupTbl
+            then Just $ LookupTable (LookupRow (queen, to) : filter ((/= from) . snd . lookupRow) lookupTbl) dims
+            else Nothing
+        Nothing -> Nothing
+
+lookupByCoordinate :: Coordinate -> [LookupRow] -> Maybe Queen
+lookupByCoordinate coord = fmap (fst . lookupRow) . find (\(LookupRow (_, c)) -> c == coord)
+
+initializeLookupTable :: Dims -> [Coordinate] -> LookupTable
+initializeLookupTable dims coords =
+  LookupTable (zipWith (curry LookupRow) queens coords) dims
+ where
+  queens = map Queen [1 .. length coords]
+
+queensToMove :: Int -> Int -> IO [Int]
+queensToMove n threats = replicateM threats (randomRIO (1, n))
+
+-- Setting up the board
+coordinateValue :: Coordinate -> (Int, Int)
+coordinateValue = bimap rank file . coordinate
+rowValue :: Coordinate -> Int
+rowValue = rank . fst . coordinate
+columnValue :: Coordinate -> Int
+columnValue = file . snd . coordinate
+
+randomNPositions :: Int -> IO [Coordinate]
+randomNPositions n = do
+  let allPossibleTuples = [(Rank i, File j) | i <- [1 .. n], j <- [1 .. n]]
+  shuffledTuples <- shuffle allPossibleTuples
+  return $ map Coordinate $ take n (nub shuffledTuples)
+ where
+  shuffle :: [a] -> IO [a]
+  shuffle xs = do
+    gen <- newStdGen
+    return $ map fst . sortBy (compare `on` snd) $ zip xs (randoms gen :: [Int])
+
+diagonalCoordinates :: BoardSize -> [Coordinate]
+diagonalCoordinates n = zipWith makeCoord [1 .. n] [1 .. n]
+ where
+  makeCoord x y = Coordinate (Rank x, File y)
+
+createDiagonalBoard :: BoardSize -> LookupTable
+createDiagonalBoard n = LookupTable diagonalLookup (n, n)
+ where
+  diagonalCoords = diagonalCoordinates n
+  diagonalLookup = zipWith makeLookupRow [1 .. n] diagonalCoords
+
+  makeLookupRow q coord = LookupRow (Queen q, coord)
+
+clsPrint :: LookupTable -> Configuration -> IO ()
+clsPrint b@(LookupTable lookupRow _) cfg = do
+  system "clear"
+  print b
+  print cfg
+  putStrLn $ "Num Threats: " ++ show (numThreats b)
+  putStr $ unlines $ map show lookupRow
+
+solved8QueensBoard :: LookupTable
+solved8QueensBoard = initializeLookupTable (8, 8) coordinates
+ where
+  coordinates =
+    [ Coordinate (Rank 1, File 6)
+    , Coordinate (Rank 2, File 4)
+    , Coordinate (Rank 3, File 7)
+    , Coordinate (Rank 4, File 1)
+    , Coordinate (Rank 5, File 8)
+    , Coordinate (Rank 6, File 2)
+    , Coordinate (Rank 7, File 5)
+    , Coordinate (Rank 8, File 3)
+    ]
+
+evolvePopulation :: MutationRate -> CrossoverRate -> Population -> Configuration -> IO Population
+evolvePopulation mutationRate crossoverRate initialPopulation cfg = do
+  let eliteSize = 5
+      sortedPop = sortOn numThreats initialPopulation
+      elites = take eliteSize sortedPop
+      nonElites = drop eliteSize sortedPop
+
+  randNonElite1 <- randomElement nonElites
+  randNonElite2 <- randomElement nonElites
+  randBoard1 <- randomNSizeBoard (boardSize cfg)
+
+  let newElites = elites ++ [randNonElite1, randNonElite2, randBoard1]
+
+  children <- replicateM (popSize - length newElites) (performCrossover sortedPop)
+
+  return $ newElites ++ children
+ where
+  popSize = length initialPopulation
+  performCrossover :: Population -> IO LookupTable
+  performCrossover population = do
+    parent1 <- randomElement population
+    parent2 <- randomElement population
+    child <- crossover crossoverRate parent1 parent2
+    mutateChild mutationRate child cfg
+
+  crossover :: CrossoverRate -> LookupTable -> LookupTable -> IO LookupTable
+  crossover rate parent1@(LookupTable _ dims1) parent2@(LookupTable _ dims2) = do
+    r <- randomRIO (0, 1)
+    if r < rate
+      then do
+        crossoverPoint <- randomRIO (1, length (lookupTable parent1) - 1)
+        let (firstHalf, _) = splitAt crossoverPoint (lookupTable parent1)
+            (_, secondHalf) = splitAt crossoverPoint (lookupTable parent2)
+
+        let firstHalfPositions = map ((coordinate . snd) . lookupRow) firstHalf
+        let secondHalfPositions = map ((coordinate . snd) . lookupRow) secondHalf
+        let collisionExists = any (`elem` firstHalfPositions) secondHalfPositions
+
+        if collisionExists
+          then return parent1
+          else return $ LookupTable (firstHalf ++ secondHalf) dims1
+      else return parent1
+
+mutateChild :: MutationRate -> LookupTable -> Configuration -> IO LookupTable
+mutateChild rate child cfg = do
+  let LookupTable tableRows dims = child
+  r <- randomRIO (0, 1)
+  if r < rate
+    then do
+      clsPrint child cfg
+      let (n, _) = dims
+      let threats = numThreats child
+      queensIndices <- queensToMove n threats
+
+      let occupiedPositions = map (coordinate . snd . lookupRow) tableRows
+      let unoccupiedPositions =
+            [(Rank r, File c) | r <- [1 .. n], c <- [1 .. n], (Rank r, File c) `notElem` occupiedPositions]
+
+      newPositions <-
+        replicateM
+          threats
+          ( if null unoccupiedPositions
+              then return (Rank 1, File 1)
+              else randomElement unoccupiedPositions
+          )
+
+      let mutated =
+            map
+              ( \row ->
+                  foldl
+                    ( \acc (i, newPos) ->
+                        updateRow i newPos acc
+                    )
+                    row
+                    (zip queensIndices newPositions)
+              )
+              tableRows
+      return $ LookupTable mutated dims
+    else return child
+ where
+  updateRow i newPos row@(LookupRow (q, Coordinate oldPos))
+    | queen q == i = LookupRow (q, Coordinate newPos)
+    | otherwise = row
+
+randomElement :: [a] -> IO a
+randomElement xs = do
+  i <- randomRIO (0, length xs - 1)
+  return $ xs !! i
+
+randomNSizeBoard :: Int -> IO LookupTable
+randomNSizeBoard n =
+  randomNPositions n <&> initializeLookupTable (n, n)
 
 main :: IO ()
 main = do
-  -- putStrLn "Hello, Haskell!"
-  let board = initializaNBoard 10
-   in print $ length board
+  args <- getArgs
+  ops <- mapM strToOps args >>= checkOps
+  let (CmdCrossover crssover) = getDefaultOrOption ops (CmdCrossover 0.7)
+      (CmdMutation mut) = getDefaultOrOption ops (CmdMutation 0.01)
+      (CmdPopulation pop) = getDefaultOrOption ops (CmdPopulation 8)
+      (CmdGeneration gen) = getDefaultOrOption ops (CmdGeneration 1000)
+      (CmdSize size) = getDefaultOrOption ops (CmdSize 8)
+      config =
+        Configuration
+          { popSize = pop
+          , mutationRate = mut
+          , maxGen = gen
+          , generationSize = 0
+          , crossOverRate = crssover
+          , boardSize = size
+          }
+
+  initialPopulation <- processInitBoard ops size pop
+  let loop acc population = do
+        newPopulation <- evolvePopulation mut crssover population (config{generationSize = acc})
+        let maybeSol = find ((== 0) . numThreats) newPopulation
+            bestFit = head $ sortOn numThreats newPopulation
+        if acc < gen
+          then case maybeSol of
+            Just solution -> do
+              putStrLn "Found a solution:"
+              putStrLn $ "Num generations taken: " ++ show acc ++ " generations"
+              print solution
+              exitSuccess
+            Nothing -> loop (acc + 1) newPopulation
+          else do
+            putStrLn ("\nFailed to find solution in " ++ show gen ++ " generations")
+            putStrLn "Best Board: "
+            print bestFit
+            print $ "Num Threats: " ++ show (numThreats bestFit)
+   in loop 0 initialPopulation
